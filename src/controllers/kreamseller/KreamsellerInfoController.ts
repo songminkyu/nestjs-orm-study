48c4cd4b61560a6e59cde303bba784f21d5b4690
songminkyu <rabbircarrot@naver.com>
2025-02-24 14:37:59 +0900

    update : 샘플 수정

--- src/controllers/kreamseller/KreamsellerInfoController.ts
+++ src/controllers/kreamseller/KreamsellerInfoController.ts
@@ -1,50 +1,24 @@
 import core from "@nestia/core";
-import { Controller, NotFoundException, Body } from "@nestjs/common";
+import { Body, Controller } from "@nestjs/common";
+
 import { ISeller } from "@ORGANIZATION/PROJECT-api/lib/structures/kreamseller/ISeller";
-import { SellerProvider } from "../../providers/kreamseller/SellerProvider";
-import { PrismaClient } from "@prisma/client";
+
+import { SellerService } from "../../providers/kreamseller/SellerProvider";
 
 @Controller("kream/sellerinfo")
 export class KreamsellerInfoController {
-    private prisma: PrismaClient;
+  private seller: SellerService;
+  constructor() {
+    this.seller = new SellerService();
+  }
 
-    constructor() {
-        this.prisma = new PrismaClient();
-    }
+  @core.TypedRoute.Get()
+  public async get(): Promise<ISeller> {
+      return await this.seller.readSeller();
+  }
 
-    /**
-     * Get performance information.
-     *
-     * Get perofmration information composed with CPU, memory and resource usage.
-     *
-     * @returns Performance info
-     * @tag Monitor
-     * @throws {NotFoundException} When seller data is not found
-     *
-     * @author Samchon
-     */
-    @core.TypedRoute.Get()
-    public async get(): Promise<ISeller> {
-        const sellerData = await this.prisma.seller.findFirst(SellerProvider.json.select());
-
-        if (!sellerData) {
-            throw new NotFoundException('Seller data not found');
-        }
-
-        return SellerProvider.json.transform(sellerData);
-    }
-
-    @core.TypedRoute.Post()
-    public async post(@Body() data: ISeller): Promise<ISeller> {
-        // Transform the input data using SellerProvider.collect
-        const createData = SellerProvider.collect(data);
-
-        // Insert the data into the database
-        const createdSeller = await this.prisma.seller.create({
-            data: createData
-        });
-
-        // Transform and return the created data
-        return SellerProvider.json.transform(createdSeller);
-    }
+  @core.TypedRoute.Post()
+  public async post(@Body() data: ISeller): Promise<ISeller> {
+      return await this.seller.createSeller(data)
+  }
 }
--- src/providers/kreamseller/SellerProvider.ts
+++ src/providers/kreamseller/SellerProvider.ts
@@ -1,6 +1,6 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { NotFoundException, Injectable } from "@nestjs/common"
 import { ISeller } from "@ORGANIZATION/PROJECT-api/lib/structures/kreamseller/ISeller";
-
 export namespace SellerProvider {
     export namespace json {
         export const transform = (
@@ -24,4 +24,35 @@ export namespace SellerProvider {
             seles_item: input.seles_item,
             created_at: new Date(),
         });
+}
+@Injectable()
+export class SellerService {
+    private prisma: PrismaClient;
+    constructor() {
+        this.prisma = new PrismaClient()
+    }
+
+    async readSeller() {
+        const sellerData = await this.prisma.seller.findFirst(
+            SellerProvider.json.select()
+        );
+
+        if (!sellerData) {
+            throw new NotFoundException('Seller data not found');
+        }
+
+        return SellerProvider.json.transform(sellerData);
+    }
+    async createSeller(data: ISeller){
+        // Transform the input data using SellerProvider.collect
+        const createData = SellerProvider.collect(data);
+
+        // Insert the data into the database
+        const createdSeller = await this.prisma.seller.create({
+            data: createData
+        });
+
+        // Transform and return the created data
+        return SellerProvider.json.transform(createdSeller);
+    }
 }
